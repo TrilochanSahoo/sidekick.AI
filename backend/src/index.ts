1101d332fd7c66c38a77f9b65cf6635051492c36
TrilochanSahoo <trilochansahoo026@gmail.com>
2025-02-10 23:33:35 +0530

    claude api setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("test")
+require("dotenv").config()
+
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+const main = async ()=>{
+    const msg = await anthropic.messages.create({
+      model: "claude-3-5-sonnet-20241022",
+      max_tokens: 1000,
+      temperature: 1,
+      messages: [{
+        role: "user",
+        content : "what is the capital of india?"
+      }]
+    });
+    console.log(msg);
+}
+
+main()
