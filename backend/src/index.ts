f06a798105870b435445f67c10eabef194e4879e
TrilochanSahoo <trilochansahoo026@gmail.com>
2025-03-08 22:56:43 +0530

    added for streaming the data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,16 +5,14 @@ import Anthropic from "@anthropic-ai/sdk";
 const anthropic = new Anthropic();
 
 const main = async ()=>{
-    const msg = await anthropic.messages.create({
-      model: "claude-3-5-sonnet-20241022",
-      max_tokens: 1000,
-      temperature: 1,
-      messages: [{
-        role: "user",
-        content : "what is the capital of india?"
-      }]
+    anthropic.messages.stream({
+        messages: [{role: 'user', content: "create a couter app"}],
+        model: 'claude-3-7-sonnet-20250219',
+        max_tokens: 1024,
+    }).on('text', (text) => {
+        console.log(text);
     });
-    console.log(msg);
+    
 }
 
 main()
